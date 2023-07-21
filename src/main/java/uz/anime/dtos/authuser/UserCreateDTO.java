1007579996a4fedeb601f184e71651e46e831cd2
elshod1018 <nuriddinovelshod2003@gmail.com>
2023-07-21 15:42:45 +0500

    Production added

--- src/main/java/uz/anime/dtos/authuser/UserCreateDTO.java
+++ src/main/java/uz/anime/dtos/authuser/UserCreateDTO.java
@@ -1,6 +1,6 @@
 package uz.anime.dtos.authuser;
 
-import jakarta.validation.constraints.Email;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -17,8 +17,9 @@ public final class UserCreateDTO implements Serializable {
     @NotBlank(message = "Username can not be blank")
     private String username;
 
-    @Pattern(regexp = "^(\\w+)@([A-Za-z0-9]{3,9})\\.([a-z0-9]{2,4})$",message = "It must be email format")
+    @Schema(example = "anime123@gmail.com")
     @NotBlank(message = "Email can not be blank")
+    @Pattern(regexp = "^(\\w+)@([A-Za-z0-9]{3,9})\\.([a-z0-9]{2,4})$", message = "It must be email format")
     private String email;
 
     @Size(min = 8, max = 16, message = "Password must be between {min} and {max} character")
@@ -28,6 +29,4 @@ public final class UserCreateDTO implements Serializable {
     @Size(min = 8, max = 16, message = "Password must be between {min} and {max} character")
     @NotBlank(message = "Confirm Password can not be blank")
     private String confirmPassword;
-
-
 }
