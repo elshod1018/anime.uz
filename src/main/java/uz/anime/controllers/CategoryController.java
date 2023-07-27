01d844894382e5c49f9ea0852902059195fe4e40
elshod1018 <nuriddinovelshod2003@gmail.com>
2023-07-27 17:35:11 +0500

    documents added

--- src/main/java/uz/anime/controllers/CategoryController.java
+++ src/main/java/uz/anime/controllers/CategoryController.java
@@ -27,6 +27,7 @@ import static uz.anime.utils.UrlUtils.BASE_CATEGORY_URL;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(BASE_CATEGORY_URL)
+@PreAuthorize("isAnonymous()")
 @Tag(name = "Category", description = "Category CRUD API")
 public class CategoryController {
     private final ObjectMapper objectMapper;
@@ -36,7 +37,7 @@ public class CategoryController {
             @ApiResponse(responseCode = "200", description = "Category created", content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
             @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = ResponseDTO.class)))})
     @PostMapping(value = "/create")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<ResponseDTO<Category>> createCategory(@Valid @RequestBody CategoryCreateDTO dto) throws JsonProcessingException {
         Category category = categoryService.create(dto);
         log.warn("Category is created : {}", objectMapper.writeValueAsString(category));
@@ -47,7 +48,7 @@ public class CategoryController {
             @ApiResponse(responseCode = "200", description = "Category returned", content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
             @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = ResponseDTO.class)))})
     @GetMapping(value = "/get/{id:.*}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
     public ResponseEntity<ResponseDTO<Category>> getCategory(@PathVariable(name = "id") Integer id) {
         return ResponseEntity.ok(new ResponseDTO<>(categoryService.findById(id)));
     }
@@ -56,7 +57,7 @@ public class CategoryController {
             @ApiResponse(responseCode = "200", description = "Category list returned", content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
             @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = ResponseDTO.class)))})
     @GetMapping(value = "/get/all")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
     public ResponseEntity<ResponseDTO<Page<Category>>> getCategoryList(@RequestParam(name = "page", defaultValue = "1") Integer page,
                                                                        @RequestParam(name = "size", defaultValue = "10") Integer size) {
         PageRequest p = PageRequest.of(page - 1, size);
@@ -67,7 +68,7 @@ public class CategoryController {
             @ApiResponse(responseCode = "200", description = "Category updated successfully", content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
             @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = ResponseDTO.class)))})
     @PutMapping(value = "/update")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<ResponseDTO<Category>> updateCategory(@RequestBody CategoryUpdateDTO dto) throws JsonProcessingException {
         Category category = categoryService.update(dto);
         log.warn("Category is updated : {}", objectMapper.writeValueAsString(category));
@@ -78,7 +79,7 @@ public class CategoryController {
             @ApiResponse(responseCode = "200", description = "Category deleted successfully", content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
             @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = ResponseDTO.class)))})
     @DeleteMapping(value = "/delete/{id:.*}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<ResponseDTO<Category>> deleteCategory(@PathVariable(name = "id") Integer id) throws JsonProcessingException {
         Category category = categoryService.delete(id);
         log.warn("Category is deleted : {}", objectMapper.writeValueAsString(category));
